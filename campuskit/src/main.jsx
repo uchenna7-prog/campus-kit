d9abd9299ba04660813278b2cf8661a29e87f4f1
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 17:38:01 +0100

    Fix import path for SideBarProvider component

--- campuskit/src/main.jsx
+++ campuskit/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { SideBarProvider } from './contexts/SideBarContext.jsx';
+import { SideBarProvider } from './contexts/SidebarContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
