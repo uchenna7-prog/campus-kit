a9ce2ac12a0db4cbe03d121e3573a13e9fdb3ae6
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 23:12:27 +0100

    Update App.jsx

--- campuskit/src/App.jsx
+++ campuskit/src/App.jsx
@@ -1,12 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import CgpaCalculator from "./pages/CgpaCalculator/CgpaCalculator";
+import PassportMaker from "./pages/PassportMaker/PassportMaker";
 
 function App() {
   return (
     <Routes>
-      {/* Root route: CGPA Calculator */}
-      <Route path="/" element={<CgpaCalculator />} />
-      {/* You can add more routes here in the future */}
+      {/* Redirect root to CGPA Calculator for now */}
+      <Route path="/" element={<Navigate to="/cgpa-calculator" replace />} />
+
+      {/* Academic Tools */}
+      <Route path="/cgpa-calculator" element={<CgpaCalculator />} />
+
+      {/* Student Utilities */}
+      <Route path="/passport-maker" element={<PassportMaker />} />
     </Routes>
   );
 }
