a8880db865fcacfeaee34b7211f0364964ca6e91
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 16:00:51 +0100

    Update main.jsx

--- campuskit/src/main.jsx
+++ campuskit/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+
+// Context Providers 🌐
+import { SideBarProvider } from './contexts/SideBarContext.jsx';
+import { ThemeProvider } from './contexts/ThemeContext.jsx';
+import { GpaProvider } from './contexts/GpaContext.jsx';
+import { PredictionProvider } from './contexts/PredictionContext.jsx';
+import { GpaCalculatorProvider } from './contexts/GpaCalculatorContext.jsx';
+import { SettingsProvider } from './contexts/SettingsContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <ThemeProvider>
+        <SettingsProvider>
+          <SideBarProvider>
+            <GpaProvider>
+              <PredictionProvider>
+                <GpaCalculatorProvider>
+                  <App />
+                </GpaCalculatorProvider>
+              </PredictionProvider>
+            </GpaProvider>
+          </SideBarProvider>
+        </SettingsProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
