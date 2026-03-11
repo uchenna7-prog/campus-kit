a87102ae9aa9f037809d34764ab6385fd4ff9561
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 16:18:52 +0100

    Update main.jsx

--- campuskit/src/main.jsx
+++ campuskit/src/main.jsx
@@ -3,23 +3,14 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-
-// Context Providers 🌐
 import { SideBarProvider } from './contexts/SideBarContext.jsx';
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      
-        
-          <SideBarProvider>
-         
-                  <App />
-           
-          </SideBarProvider>
-     
-     
+      <SideBarProvider>
+        <App />
+      </SideBarProvider>
     </BrowserRouter>
   </StrictMode>
 );
