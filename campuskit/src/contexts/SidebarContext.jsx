29e6097f81b59ba3a4ec5be4b9801ca64d92af8d
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 16:09:15 +0100

    Update SidebarContext.jsx

--- campuskit/src/contexts/SidebarContext.jsx
+++ campuskit/src/contexts/SidebarContext.jsx
@@ -1,1 +1,50 @@
-...
+import { createContext,useContext,useState,useEffect } from "react";
+
+const sideBarContext = createContext();
+
+export default sideBarContext;
+
+function SideBarProvider({ children }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [isMobile, setIsMobile] = useState(false)
+
+    const handleResize = () =>{
+        if (window.innerWidth < 768){
+            setIsMobile(true)
+        }
+        else{
+            setIsMobile(false)
+        }
+    }
+
+    const toggleSideBar = () => {
+        if (isMobile){
+            setIsOpen(!isOpen);
+        }
+        else{
+            setIsCollapsed(!isCollapsed)
+        }
+
+    }
+
+    useEffect(()=>{
+
+        handleResize()
+        window.addEventListener("resize",handleResize)
+
+        return () => window.removeEventListener("resize",handleResize)
+
+    },[])
+
+    return(
+        <sideBarContext.Provider value={{ isOpen, isCollapsed, isMobile, toggleSideBar }}>
+            {children}
+        </sideBarContext.Provider>
+    )
+}
+
+export { SideBarProvider };
+
+export const useSideBarContext = () => useContext(sideBarContext);
