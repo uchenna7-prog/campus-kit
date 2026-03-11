5b025f45f33577a5ab3cccab942886833178b3a4
uchendu uchenna Daniel <uchenduuchenna7@gmail.com>
2026-03-11 16:18:25 +0100

    Update App.jsx

--- campuskit/src/App.jsx
+++ campuskit/src/App.jsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
-// Updated import to reflect the new component name
 import CgpaCalculator from './pages/CgpaCalculator/CgpaCalculator';
 
 function App() {
   return (
     <Routes>
-     
       <Route path="/" element={<CgpaCalculator />} />
-      
-     
+      <Route path="/cgpa-calculator" element={<CgpaCalculator />} />
     </Routes>
   );
 }
